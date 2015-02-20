b0c24be390482710cb558182c5b7035886a0d8e6
Silas Baronda <silas.baronda@gmail.com>
2015-02-20 18:19:45 -0500

    Updating package and README information

--- setup.py
+++ setup.py
@@ -23,13 +23,13 @@ plugin_version = "0.1"
 plugin_description = "TODO"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "TODO"
+plugin_author = "Silas Baronda"
 
 # The plugin's author's mail address.
 plugin_author_email = "silas.baronda@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "TODO"
+plugin_url = "http://github.com/silasb/OctoPrint-Spool-Management"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
